297724616cf827aeda19a72b241f661864ba30b3
Tushar Singh <101038096+tushars2610@users.noreply.github.com>
2024-07-25 11:32:49 +0530

    commit

--- Page/script.js
+++ Page/script.js
@@ -28,3 +28,13 @@ function openNav() {
     document.getElementById("open_menu").style.marginLeft = "0px";
   }
   
+  document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('displayText').textContent = e.target.result;
+        };
+        reader.readAsText(file);
+    }
+});
