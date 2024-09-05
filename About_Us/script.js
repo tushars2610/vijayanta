97b8d6f224c0b7869851e28d154503d549a0b76a
Shruti Kumari <shrutikkpt80@gmail.com>
2024-09-06 00:31:35 +0530

    New About

---
+++ About_Us/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const texts = document.getElementById('historyDescription');
+    const buttons = document.getElementById('btn-1');
+    const img1 = document.getElementById('img-1');
+
+    buttons.addEventListener('click', () => {
+        if (texts.classList.contains('expanded')) {
+            texts.classList.remove('expanded');
+            buttons.textContent = 'Read More';
+            // img1.style.display='block';
+
+        } else {
+            texts.classList.add('expanded');
+            buttons.textContent = 'Read Less';
+            
+        }
+    });
+});
