f77fa00aecc19dad1de7189dcc22fb59ee4b1ff6
Shruti Kumari <shrutikkpt80@gmail.com>
2024-09-13 01:23:44 +0530

    change

---
+++ try/script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-button').addEventListener('click', function() {
+    const navbarLinks = document.getElementById('navbar-links');
+    navbarLinks.classList.toggle('active');
+});
