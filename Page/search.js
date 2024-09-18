cb6d3256f626451f669894501f9cb7fea97cd099
Shruti Kumari <115400404+itscodersk@users.noreply.github.com>
2024-09-18 17:20:28 +0530

    Update search.js

--- Page/search.js
+++ Page/search.js
@@ -1,3 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
 
   //-----------------------------search button-----------------------
   const searchDialog = document.getElementById("search-dialog")
