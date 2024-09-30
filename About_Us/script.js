bfbebed3a48d9a31637f0e0d52426fec81021e94
Tushar Singh <101038096+tushars2610@users.noreply.github.com>
2024-10-01 00:25:20 +0530

    commit

--- About_Us/script.js
+++ About_Us/script.js
@@ -37,3 +37,21 @@ document.addEventListener("click", function(event) {
       });
     }
   });
+
+
+  function toggleHamburger() {
+    var hamburger = document.getElementById('hamburger');
+    var navLinks = document.getElementById('nav-links');
+    hamburger.classList.toggle('change');
+    if (navLinks.classList.contains('hidden')) {
+        navLinks.classList.remove('hidden');
+        navLinks.classList.remove('slide-out');
+        navLinks.classList.add('slide-in');
+    } else {
+        navLinks.classList.remove('slide-in');
+        navLinks.classList.add('slide-out');
+        setTimeout(function() {
+            navLinks.classList.add('hidden');
+        }, 500); // Match the duration of the slide-out animation
+    }
+}
--- Products/index_nav_script.js
+++ Products/index_nav_script.js
@@ -1,10 +1,19 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
+function toggleHamburger() {
+    var hamburger = document.getElementById('hamburger');
+    var navLinks = document.getElementById('nav-links');
+    hamburger.classList.toggle('change');
+    if (navLinks.classList.contains('hidden')) {
+        navLinks.classList.remove('hidden');
+        navLinks.classList.remove('slide-out');
+        navLinks.classList.add('slide-in');
+    } else {
+        navLinks.classList.remove('slide-in');
+        navLinks.classList.add('slide-out');
+        setTimeout(function() {
+            navLinks.classList.add('hidden');
+        }, 500); // Match the duration of the slide-out animation
+    }
+}
 const searchDialog = document.getElementById("search-dialog")
 
 function show(){
