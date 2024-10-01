ff5371b0ec34263a8a40ab320538062c7d40b6d4
Tushar Singh <101038096+tushars2610@users.noreply.github.com>
2024-10-01 06:29:37 +0530

    commit

--- Helpdesk/search.js
+++ Helpdesk/search.js
@@ -71,3 +71,19 @@ hamburger.addEventListener('click', () => {
       resultsDiv.innerHTML = JSON.stringify(results, null, 2);
   }
   
+  function toggleHamburger() {
+    var hamburger = document.getElementById('hamburger');
+    var navLinks = document.getElementById('nav-links');
+    hamburger.classList.toggle('change');
+    if (navLinks.classList.contains('hidden')) {
+        navLinks.classList.remove('hidden');
+        navLinks.classList.remove('slide-out');
+        navLinks.classList.add('slide-in');
+    } else {
+        navLinks.classList.remove('slide-in');
+        navLinks.classList.add('slide-out');
+        setTimeout(function() {
+            navLinks.classList.add('hidden');
+        }, 500); // Match the duration of the slide-out animation
+    }
+}
