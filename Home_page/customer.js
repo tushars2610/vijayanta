7ebe6a8284015d2cd90b4a2a3bfa8b5d6e35c483
Tushar Singh <101038096+tushars2610@users.noreply.github.com>
2024-10-05 07:41:37 +0530

    commit

--- Home_page/customer.js
+++ Home_page/customer.js
@@ -1,25 +1,110 @@
-// Function to populate the logos
-function populateLogos(data) {
-    const logosSlide = document.getElementById('logos-slide');
-  
-    data.forEach(logo => {
-      const img = document.createElement('img');
-      img.src = `../testimonials/images/${logo.image}`;
-      img.alt = logo.Name;
-      logosSlide.appendChild(img);
-      
-      
-    });
-  
-    // Clone the logos-slide div to create the continuous loop effect
-    const copy = logosSlide.cloneNode(true);
-    // document.querySelector('.logos').appendChild(copy);
-  }
-  
-  // Fetch the JSON data and populate logos
-  fetch('data.json')
-    .then(response => response.json())
-    .then(data => populateLogos(data))
-    .catch(error => console.error('Error fetching the JSON data:', error));
 
-    
+// Fetch the JSON data
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        // Get the container element
+        const customerImagesContainer = document.getElementById('customer-images');
+
+        // Clear the existing images
+        customerImagesContainer.innerHTML = '';
+
+        // Loop through the JSON data and create new img elements
+        data.forEach(customer => {
+            const imagePath = `../testimonials/images/${customer.image}`;
+            const imgElement = document.createElement('img');
+            imgElement.src = imagePath;
+            imgElement.alt = '';
+            imgElement.classList.add('mr-2');
+            customerImagesContainer.appendChild(imgElement);
+        });
+    });
+
+    // Fetch the JSON data
+fetch('data.json')
+.then(response => response.json())
+.then(data => {
+    // Get the container element
+    const customerImagesContainer = document.getElementById('customer-images2');
+
+    // Clear the existing images
+    customerImagesContainer.innerHTML = '';
+
+    // Loop through the JSON data and create new img elements
+    data.forEach(customer => {
+        const imagePath = `../testimonials/images/${customer.image}`;
+        const imgElement = document.createElement('img');
+        imgElement.src = imagePath;
+        imgElement.alt = '';
+        imgElement.classList.add('mr-2');
+        customerImagesContainer.appendChild(imgElement);
+    });
+});
+
+
+
+
+
+
+
+    // ---------------------type-of-product-------------
+// Get the links-container1 element
+const linksContainer1 = document.getElementById('links-container1');
+
+// Fetch data from the JSON file
+fetch('product.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create HTML elements
+    let productHTML = '';
+    data.forEach(product => {
+      productHTML += `
+        <a href="${product.productLink}">${product.productName}</a><br>
+      `;
+    });
+    // Append the HTML to the links-container1 element
+    linksContainer1.innerHTML = productHTML;
+  })
+  .catch(error => console.error('Error:', error));
+
+
+
+
+  // Get the links-container1 element
+const linksContainer2 = document.getElementById('links-container2');
+
+// Fetch data from the JSON file
+fetch('product.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create HTML elements
+    let productHTML = '';
+    data.forEach(product => {
+      productHTML += `
+        <a href="${product.productLink}">${product.productName}</a><br>
+      `;
+    });
+    // Append the HTML to the links-container1 element
+    linksContainer2.innerHTML = productHTML;
+  })
+  .catch(error => console.error('Error:', error));
+
+
+  // Get the links-container1 element
+const linksContainer3 = document.getElementById('links-container3');
+
+// Fetch data from the JSON file
+fetch('product.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create HTML elements
+    let productHTML = '';
+    data.forEach(product => {
+      productHTML += `
+        <a href="${product.productLink}">${product.productName}</a><br>
+      `;
+    });
+    // Append the HTML to the links-container1 element
+    linksContainer3.innerHTML = productHTML;
+  })
+  .catch(error => console.error('Error:', error));
